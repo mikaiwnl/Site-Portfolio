e40954fb3ccc5ae725572d582b7c7abf77bfaaec
Michael Ali soeu <takumisoeu@gmail.com>
2024-11-28 02:25:54 -0500

    first commit

---
+++ js/scroll.js
@@ -0,0 +1,7 @@
+function scrollToCompetences() {
+    document.getElementById('competences').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToTravaux() {
+    document.getElementById('travaux').scrollIntoView({ behavior: 'smooth' });
+}
